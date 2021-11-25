7a9cb310ab082274acea7c09bc019545694599c1
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-11-25 03:14:46 +0000

    1 erro apenas...

--- Principal.java
+++ Principal.java
@@ -1,14 +1,20 @@
 public class Principal{
+    public static void main(String[] args) {
+   
 
     FiguraGeometrica f=new FiguraGeometrica();
-    f.setNomeFigura("Quadrado");
-    f.setArea(4);
+    f.setNomeFigura("quadrado");
+    f.setArea();
     f.setPerimetro();
 
     Retangulo r=new Retangulo();
     r.setLado(4);
+    
+    //r.retangulo2();
+    System.out.println("Segundo informações digitadas...");
+    r.quadrado(int);
 
-
+    }
 
 }
     
--- Retangulo.java
+++ Retangulo.java
@@ -1,13 +1,35 @@
-public class Retangulo {
+public class Retangulo extends FiguraGeometrica{
+    @Override
 
     public int lado;
-
 
     public int getLado() {
         return lado;
     }public void setLado(int lado) {
         this.lado = lado;
     }
-@overrride
+
+
+public void quadrado(Object quadrado){
+    if(lado==4){
+        quadrado=((lado/lado)*lado);
+        System.out.println("O area do "+getNomeFigura()+"é de: A"+getQuadrado());
+        System.out.println("E seu "+getPerimetro()+"é de: P"+getQuadrado());
+        }else{
+            System.out.println("Status de aprovação: Reprovado.");
+        }
+
+// public void retangulo2(Objetc retangulo2){
+//     Objetc retangulo = retangulo2;
+//     if(nomeFigura==retangulo){
+//         retangulo=((lado/lado)*lado);
+//         System.out.println("O area do "+getNomeFigura()+"é de: A"+getAreaRetangulo(int));
+//         System.out.println("E seu "+getPerimetro()+"é de: P"+getRetangulo(int));
+//         }
+//     }
+}
+private String getQuadrado() {
+    return null;
+}
     
 }
