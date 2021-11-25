e75037329d30154d7ce4a1f0a64fa466013cedd8
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-11-25 01:05:09 +0000

    Falta aprimorar...

---
+++ Principal.java
@@ -0,0 +1,14 @@
+public class Principal{
+
+    FiguraGeometrica f=new FiguraGeometrica();
+    f.setNomeFigura("Quadrado");
+    f.setArea(4);
+    f.setPerimetro();
+
+    Retangulo r=new Retangulo();
+    r.setLado(4);
+
+
+
+}
+    
---
+++ Retangulo.java
@@ -0,0 +1,13 @@
+public class Retangulo {
+
+    public int lado;
+
+
+    public int getLado() {
+        return lado;
+    }public void setLado(int lado) {
+        this.lado = lado;
+    }
+@overrride
+    
+}
